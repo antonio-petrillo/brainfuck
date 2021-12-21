d201d39c1854c80b2501887d2f5e02630a508263
antonio-petrillo <antonio.petrillo4@studenti.unina.it>
2021-12-21 15:57:04 +0100

    adding c & elisp implementation

---
+++ c_version/brainfuck.c
@@ -0,0 +1,124 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MEM_SIZE 30000
+
+typedef struct brainfuck_t {
+  char memory[MEM_SIZE];
+  unsigned short pointer;
+  char *program;
+  unsigned short ic;
+} * brainfuck_t;
+
+brainfuck_t init();
+void destroy(brainfuck_t *bf);
+
+void execute_program(brainfuck_t bf, char *program);
+void execute_cycle(brainfuck_t bf, char instr);
+
+int main(int argc, char **argv) {
+  brainfuck_t bf = init();
+  // hello world
+  execute_program(bf, "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---"
+                      ".+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
+  // test: see http://brainfuck.org/tests.b
+  execute_program(bf, "++++[>+++++<-]>[<+++++>-]+<+[>[>+>+<<-]++>>[<<+>>-]>>>[-"
+                      "]++>[-]+>>>+[[-]++++"
+                      "++>>>]<<<[[<++++++++<++>>-]+<.<[>----<-]<]<<[>>>>>[>>>[-"
+                      "]+++++++++<[>-<-]+++"
+                      "++++++>[-[<->-]+[<<<]]<[>+<-]>]<<-]<<-]");
+
+  destroy(&bf);
+  return 0;
+}
+
+brainfuck_t init() {
+  brainfuck_t bf = (brainfuck_t)malloc(sizeof(struct brainfuck_t));
+  assert(bf);
+  memset(bf->memory, 0, MEM_SIZE);
+  bf->pointer = 0;
+  bf->ic = 0;
+  bf->program = NULL;
+  return bf;
+}
+
+void destroy(brainfuck_t *bf) {
+  assert(*bf);
+  free(*bf);
+  bf = NULL;
+}
+
+void execute_program(brainfuck_t bf, char *program) {
+  assert(bf);
+  if (bf->program) {
+    free(bf->program);
+    bf->program = NULL;
+  }
+  size_t program_len = strlen(program) + 1;
+  bf->program = (char *)calloc(program_len, sizeof(char));
+  assert(bf->program);
+  strncpy(bf->program, program, program_len);
+  char current_instruction;
+  while ((current_instruction = bf->program[bf->ic]) != '\0') {
+    execute_cycle(bf, current_instruction);
+  }
+}
+
+void execute_cycle(brainfuck_t bf, char instr) {
+  assert(bf);
+  switch (instr) {
+  case '+':
+    bf->memory[bf->pointer]++;
+    break;
+  case '-':
+    bf->memory[bf->pointer]--;
+    break;
+  case '.':
+    putchar(bf->memory[bf->pointer]);
+    break;
+  case ',':
+    bf->memory[bf->pointer] = getchar();
+    getchar();
+    break;
+  case '>':
+    bf->pointer++;
+    break;
+  case '<':
+    bf->pointer--;
+    break;
+  case '[':
+    if (bf->memory[bf->pointer] == 0) {
+      unsigned short num_of_bracket = 0;
+      char finished = 0;
+      while (!finished) {
+        bf->ic++;
+        if (bf->program[bf->ic] == ']') {
+          num_of_bracket == 0 ? finished = 1 : num_of_bracket--;
+        } else if (bf->program[bf->ic] == '[') {
+          num_of_bracket++;
+        }
+      }
+    }
+    break;
+  case ']':
+    if (bf->memory[bf->pointer] != 0) {
+      unsigned short num_of_bracket = 0;
+      char finished = 0;
+      while (!finished) {
+        bf->ic--;
+        if (bf->program[bf->ic] == '[') {
+          num_of_bracket == 0 ? finished = 1 : num_of_bracket--;
+        } else if (bf->program[bf->ic] == ']') {
+          num_of_bracket++;
+        }
+      }
+    }
+    break;
+    // default:
+    // line of comment do nothing
+  }
+  bf->ic++;
+}
---
+++ elisp-version/brainfuck.el
@@ -0,0 +1,101 @@
+;;; brainfuck.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Antonio Petrillo
+;;
+;; Author: Antonio Petrillo <https://github.com/anto>
+;; Maintainer: Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
+;; Created: December 21, 2021
+;; Modified: December 21, 2021
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/anto/brainfuck
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+(defun brainfuck-execute-program (program)
+  "Interpret the brainfuck PROGRAM given in input."
+  (let ((program-len (1- (length program)))
+        (pointer 0)
+        (memory (make-vector 30000 0))
+        (ic 0)
+        (bf-out (get-buffer-create "brainfuck-output"))
+        (bf-message (get-buffer-create "brainfuck-message")))
+
+    (with-current-buffer bf-message
+      (delete-region (point-min) (point-max)))
+
+    (with-current-buffer bf-out
+      (delete-region (point-min) (point-max)))
+
+    (with-current-buffer bf-message
+      (insert "--------------------\nExecuting:\n\n" program "\n\n--------------------\n"))
+
+    (while (<= ic program-len)
+      (let ((current-instr (aref program ic)))
+
+        (with-current-buffer bf-message
+          (insert "ic: " (number-to-string ic) "\t instr: " current-instr "\n"))
+
+        (cond ((eq current-instr ?+)
+               (aset memory pointer (1+ (aref memory pointer))))
+
+              ((eq current-instr ?-)
+               (aset memory pointer (1- (aref memory pointer))))
+
+              ((eq current-instr ?.)
+               (let ((byte-out (aref memory pointer)))
+                 (with-current-buffer bf-out
+                   (insert byte-out))))
+
+              ((eq current-instr ?,)
+               (aset memory pointer (read-char)))
+
+              ((eq current-instr ?>)
+               (setq pointer (1+ pointer)))
+
+              ((eq current-instr ?<)
+               (setq pointer (1- pointer)))
+
+              ((eq current-instr ?\[)
+               (if (eq (aref memory pointer) 0)
+                   (let ((num-of-bracket 0)
+                         (continue t))
+                     (while (eq continue t)
+                       (setq ic (1+ ic))
+                       (cond ((eq (aref program ic) ?\[)
+                              (setq num-of-bracket (1+ num-of-bracket)))
+                             ((eq (aref program ic) ?\])
+                              (if (eq num-of-bracket 0)
+                                  (setq continue nil)
+                                (setq num-of-bracket (1- num-of-bracket)))))))))
+
+              ((eq current-instr ?\])
+               (if (not (eq (aref memory pointer) 0))
+                   (let ((num-of-bracket 0)
+                         (continue t))
+                     (while (eq continue t)
+                       (setq ic (1- ic))
+                       (cond ((eq (aref program ic) ?\])
+                              (setq num-of-bracket (1+ num-of-bracket)))
+                             ((eq (aref program ic) ?\[)
+                              (if (eq num-of-bracket 0)
+                                  (setq continue nil)
+                                (setq num-of-bracket (1- num-of-bracket)))))))))
+
+              (t (with-current-buffer bf-message
+                   (insert current-instr)))))
+      (setq ic (1+ ic)))
+    (message "\n\nExecution ended\n--------------------\n")))
+
+(brainfuck-execute-program "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+
+(provide 'brainfuck)
+;;; brainfuck.el ends here
